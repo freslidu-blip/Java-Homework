317aed0359ed7d62023a47386025ada1c4833de0
freslidu-blip <freslidu@gmail.com>
2026-09-07 01:13:14 +0500

    Add files via upload

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 18;
+        double height = 1.70;
+        boolean student = true;
+        String name = "Дима";
+        char grade = 'A';
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(student);
+        System.out.println(name);
+        System.out.println(grade);
+    }
+
+}
