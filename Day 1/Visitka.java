59c1943ff97641eda558e09362fcf67a5f047971
freslidu-blip <freslidu@gmail.com>
2026-09-12 02:08:45 +0500

    Add files via upload

---
+++ Day 1/Visitka.java
@@ -0,0 +1,7 @@
+public class Visitka {
+    public static void main(String[] args) {
+        System.out.println("Имя: Дима");
+        System.out.println("Возраст: 18");
+        System.out.println("Город: Караганда");
+    }
+}
