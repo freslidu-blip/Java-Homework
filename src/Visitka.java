4f46c75b9c829ba75cd486d273e286ad2c0d1676
freslidu-blip <freslidu@gmail.com>
2026-09-12 02:07:49 +0500

    Delete src directory

--- src/Visitka.java
+++
@@ -1,7 +0,0 @@
-public class Visitka {
-    public static void main(String[] args) {
-        System.out.println("Имя: Дима");
-        System.out.println("Возраст: 18");
-        System.out.println("Город: Караганда");
-    }
-}
